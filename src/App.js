4ecbd96d883c085454a140981beaa5a95c274669
ObaidNadeem <obaidnadeem4@gmail.com>
2021-08-26 13:21:08 +0500

    useWindowSize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useWindowSize} from './useWindowSize/useWindowSize'
+
 function App() {
+
+  const [height, width] = useWindowSize()
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is the height of the screen  {height}
+      <div></div>
+      This is the width of the screen  {width} 
     </div>
   );
 }
